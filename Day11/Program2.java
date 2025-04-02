8f149225c96018e72042a655b0565da9526bb465
Tejashwini0123 <tejashwinichary67@gmail.com>
2025-04-02 12:41:59 +0530

    Update Program2.java

--- Day11/Program2.java
+++ Day11/Program2.java
@@ -40,26 +40,16 @@ Sample Output-2:
 
 */
 import java.util.*;
-public class Test {
- public static int minSum(int N) {
-        int[] digits = new int[4];
-        for (int i = 3; i >= 0; i--) {
-            digits[i] = N % 10;
-            N /= 10;
-}
-        Arrays.sort(digits);
-        int N1 = 0, N2 = 0;
-        for (int i = 0; i < 4; i++) {
-            if (i % 2 == 0) {
-                N1 = N1 * 10 + digits[i]; 
-} else{
-    N2 = N2 * 10 + digits[i];  
-   }
-}       return N1 + N2;
-}
-   public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n=sc.nextInt();
-        System.out.println(minSum(n));
-}
+class Solution {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        char c[]=s.toCharArray();
+        Arrays.sort(c);
+        String s1=c[0]+""+c[3];
+        String s2=c[1]+""+c[2];
+        int x=Integer.parseInt(s1);
+        int y=Integer.parseInt(s2);
+        System.out.println(x+y);
+    }
 }
